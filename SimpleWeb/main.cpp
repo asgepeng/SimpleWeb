d42e9569a9db4dd9a2b2178656f8e80234ccf9a5
Muhammad Asrori <asgepeng@gmail.com>
2025-05-29 16:10:40 +0700

    Naming convertion

--- SimpleWeb/main.cpp
+++ SimpleWeb/main.cpp
@@ -5,20 +5,14 @@
 
 int main()
 {
-    std::cout << "Current Directory: "
-        << std::filesystem::current_path() << std::endl;
-
     Web::Server app;
     Web::ControllerRoutes routing;
 
     app.MapController(routing);
 
-    if (!app.Run())
-    {
-        return 1;
-    }
+    if (!app.Run()) return 1;
 
-    std::cout << "Server running on http://localhost/\nPress Enter to exit.\n";
+    std::cout << "Server running on https://localhost/\nPress Enter to exit.\n";
     std::string temp;
     std::getline(std::cin, temp);
     app.Stop();
